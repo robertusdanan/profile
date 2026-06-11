3ab4513e16c0fc8df0609955a173cd77c28d6810
robertusdanan <rdananhp@gmail.com>
2026-06-11 10:09:26 +0700

    fbid

--- index.php
+++ index.php
@@ -119,6 +119,7 @@ $og_image       = $og_url . 'og.jpg';
 <meta name="twitter:title"       content="<?= $og_title ?>"/>
 <meta name="twitter:description" content="<?= $og_description ?>"/>
 <meta name="twitter:image"       content="<?= $og_image ?>"/>
+<meta property="fb:app_id" content="2644601879268512" />
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Sora:wght@300;400;500;600&family=JetBrains+Mono:wght@300;400&display=swap" rel="stylesheet"/>
 <style>
